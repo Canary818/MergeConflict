1b92a9af888595febb1e5a1e23aaa77d1532525c
technicalwaffle <technicalwaffle@gmail.com>
2026-04-23 18:58:56 -0700

    Basic dialogue system and typewriter done

---
+++ Assets/Scripts/ReferenceManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class ReferenceManager : MonoBehaviour
+{
+    //Singleton Stuff
+    public static ReferenceManager Instance { get; private set; }
+    //Refs
+    public DialogueManager dialogueManager;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("More than 1 ReferenceManager in this scene! That's messed up fix it!!!");
+        }
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
